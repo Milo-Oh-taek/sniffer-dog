f1210c9698253765dd955d230b4bea40bcbd1240
milo <oet112@naver.com>
2022-01-15 16:16:59 +0900

    letters modified

--- front/components/layout/MainContainer.jsx
+++ front/components/layout/MainContainer.jsx
@@ -116,23 +116,9 @@ const MainContainer = () => {
 						<div style={{ float: "right" }}>
 							<Link href="/perfume/7">
 								<a className="nav-link active" aria-current="page">
-										Go
-										<br />
-										Detail
-										<br />
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width="100"
-											height="16"
-											fill="currentColor"
-											className="bi bi-arrow-right"
-											viewBox="0 0 16 16"
-										>
-											<path
-												fillRule="evenodd"
-												d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-											/>
-										</svg>
+									learn more &gt;&gt;&gt;
+									<br />
+									<br />
 								</a>
 							</Link>
 						</div>
