fbdafa8dc003f7b59668b5f2946b04885f30de1a
milo <oet112@naver.com>
2022-01-18 21:58:57 +0900

    css files

--- front/pages/_app.jsx
+++ front/pages/_app.jsx
@@ -2,9 +2,10 @@ import Layout from '../components/layout/Layout';
 import React from 'react';
 import { wrapper } from '../store';
 import 'bootstrap/dist/css/bootstrap.css';
+
 import '../components/perfume/Paginator.css';
 
-import '../components/layout/MainContainer.css';
+import '../styles/MainContainer.css';
 
 function MyApp({ Component, pageProps }) {
   return (
